daa5ede97995fa5b9389e73eaf710eeed0ea4724
Fifciuu <filip.jdrasik@gmail.com>
2019-09-01 15:25:54 +0200

    Almost done. Only Yotpo reviews amount todo

--- src/modules/magento-menus/store/actions.ts
+++ src/modules/magento-menus/store/actions.ts
@@ -18,24 +18,36 @@ export const actions: ActionTree<MenusState, any> = {
     }
   },
 
-  async loadNodes({ state, commit }, identifier) {
+  async loadNodes({ state, commit }, identifiers) {
     const { storeCode } = currentStoreView();
-    const menu = state.menus.find(v => v.identifier === identifier);
-    if (!menu || !menu.menu_id) {
-      Logger.warn('[MagentoMenus] Requested identifier does not exist');
-      return;
-    }
-    try {
-      let r = await fetch(
-        `${config.api.url}ext/menus/nodes/${storeCode}/${menu.menu_id}`
-      );
-      let { result } = await r.json();
-      commit(types.SET_MENU_ITEMS, {
-        menuId: menu.menu_id,
-        items: result
-      });
-    } catch (err) {
-      console.log(err);
+    const fetchIdentifier = async identifier => {
+      const menu = state.menus.find(v => v.identifier === identifier);
+      if (!menu || !menu.menu_id) {
+        Logger.warn('[MagentoMenus] Requested identifier does not exist');
+        return;
+      }
+      try {
+        let r = await fetch(
+          `${config.api.url}ext/menus/nodes/${storeCode}/${menu.menu_id}`
+        );
+        let { result } = await r.json();
+        commit(types.SET_MENU_ITEMS, {
+          menuId: menu.menu_id,
+          items: result
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (Array.isArray(identifiers)) {
+      const requests = [];
+      for (let identifier of identifiers) {
+        requests.push(fetchIdentifier(identifier));
+      }
+      await Promise.all(requests);
+    } else {
+      await fetchIdentifier(identifiers);
     }
   }
 };
