5a0590d4553cd2dc6c23a17f06da2a9be5746b5d
Fifciuu <filip.jdrasik@gmail.com>
2019-09-08 13:51:08 +0200

    Autofetching preconfigured products

--- src/modules/custom-packs/store/actions.ts
+++ src/modules/custom-packs/store/actions.ts
@@ -7,6 +7,7 @@ import config from 'config'
 import { currentStoreView } from '@vue-storefront/core/lib/multistore';
 import { quickSearchByQuery } from '@vue-storefront/core/lib/search';
 import { buildFilterProductsQuery } from '@vue-storefront/core/helpers';
+import builder from 'bodybuilder'
 
 const urlWithSlash = (url: string) => {
   return url.endsWith('/') ? url : url+'/'
@@ -23,33 +24,44 @@ const getCurrentStoreCode = () => {
 export const actions: ActionTree<PacksState, any> = {
 
   // Loads data for configurator with provided packId - it is being saved in the cache
-  async loadConfigurator ({ commit }, { packId, useCache = true }) {
+  async loadConfigurator ({ dispatch, commit }, { packId, useCache = true, fetchPreconfigured = true }) {
+
+    let ids = []
 
     try {
       let response = await fetch(`${urlWithSlash(config.api.url)}ext/custom-packs/creator/${packId}/${getCurrentStoreCode()}`)
       let { result } = await response.json()
-      console.log(result)
 
-      if (useCache) cacheStorage.setItem(`configurator-${packId}`, result)
+      if (useCache) cacheStorage.setItem(`configurator-${packId}`, Array.isArray(result) ? result[0] : result)
+      if (fetchPreconfigured) ids = Array.isArray(result) ? result[0].preconfigured : result.preconfigured
 
       commit(types.SET_CONFIGURATION, {
         packId,
-        configuration: result
+        configuration: Array.isArray(result) ? result[0] : result
       })
+
     } catch (err) {
       console.error('[CustomPacks] Couldn\'t fetch category\'s available packs', err)
+
       const configuration = cacheStorage.getItem(`configurator-${packId}`)
       if (!configuration) {
         return
       }
+      if (fetchPreconfigured) ids = configuration.preconfigured
+
       commit(types.SET_CONFIGURATION, {
         packId,
         configuration
       })
+
     }
 
-    const userData = cacheStorage.getItem('user')
-    return userData
+    if (fetchPreconfigured) {
+      await dispatch('loadPreconfiguredPacks', {
+        ids,
+        packId
+      })
+    }
   },
 
   // 
@@ -70,7 +82,36 @@ export const actions: ActionTree<PacksState, any> = {
 
       console.log(response)
   
-      commit(types.SET_AVAILABLE_PRODUCTS, response)
+      commit(types.SET_AVAILABLE_PRODUCTS, response.items)
+      
+    } catch (err) {
+      console.error(err)
+    }
+  },
+
+  // Load preconfigured packs by IDs
+  async loadPreconfiguredPacks (context, { ids, packId, size = 4000, start = 0, sort = 'position:asc', includeFields = config.entities.optimize ? config.entities.category.includeFields : null }) {
+    const commit = context.commit
+
+    let searchQuery = builder().query('terms', 'id', ids)
+      .filter('terms', 'status', [0, 1])
+      .filter('terms', 'visibility', [2, 3, 4])
+      .build()
+
+    try {
+      const response = await quickSearchByQuery({ 
+        entityType: 'product', 
+        query: searchQuery, 
+        sort: sort, 
+        size: size, 
+        start: start, 
+        includeFields: includeFields 
+      })
+  
+      commit(types.SET_PRECONFIGURED_PRODUCTS, {
+        packId,
+        products: response.items
+      })
       
     } catch (err) {
       console.error(err)
--- src/modules/custom-packs/store/mutations.ts
+++ src/modules/custom-packs/store/mutations.ts
@@ -2,13 +2,31 @@ import { MutationTree } from 'vuex'
 import * as types from './mutation-types'
 
 export const mutations: MutationTree<any> = {
+
   [types.SET_CONFIGURATION] (state, { packId, configuration }) {
     state.packOptions = {
       ...state.packOptions,
       [packId]: configuration
     }
   },
+
   [types.SET_AVAILABLE_PRODUCTS] (state, payload) {
     // state.users.push(payload)
+  },
+
+  [types.SET_PRECONFIGURED_PRODUCTS] (state, { packId, products }) {
+    if (!state.packOptions.hasOwnProperty(packId) || !state.packOptions[packId].preconfigured) {
+      console.error(`[CustomPacks] PackId ${packId} could not be found`)
+      return
+    }
+
+    state.packOptions = {
+      ...state.packOptions,
+      [packId]: {
+        ...state.packOptions[packId],
+        preconfigured: state.packOptions[packId].preconfigured.map(v => products.find(product => product.id === v))
+      }
+    }
+
   }
 }
