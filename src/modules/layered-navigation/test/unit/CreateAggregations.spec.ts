f8bc1e12f2f47e11c569702df5655fbbcde40b7f
Fifciuu <filip.jdrasik@gmail.com>
2019-10-08 20:37:02 +0200

    Working on aggregations creator

--- src/modules/layered-navigation/test/unit/CreateAggregations.spec.ts
+++ src/modules/layered-navigation/test/unit/CreateAggregations.spec.ts
@@ -2,8 +2,48 @@ import CreateAggregations from '../../util/CreateAggregations'
 
 describe('CreateAggregations', () => {
 
-  it('it returns base aggs object', () => {
-    expect(CreateAggregations({})).toEqual({aggs:{}})
+  it('it creates instance of CreateAggregations class', () => {
+    const instance = new CreateAggregations([])
+    expect(instance instanceof CreateAggregations).toBeTruthy()
+  })
+
+  it('it creates aggregation with one foreign filter', () => {
+    const instance = new CreateAggregations([
+      {
+        name: 'color',
+        path: 'configurable_children.color_group',
+        aggName: 'colors',
+        value: [2, 15]
+      },
+      {
+        name: 'style',
+        path: 'configurable_children.style',
+        aggName: 'styles',
+        value: [3]
+      }
+    ])
+    
+    const colorAgg = instance.appendFilterToAggregation('color')
+    expect(colorAgg).toEqual({
+        colors_wrapper: {
+          filter: {
+            terms: {
+              "configurable_children.style": [3]
+            }
+          },
+          aggs: {
+            colors: {
+              terms: {
+                field: [
+                  "configurable_children.color_group"
+                ]
+              }
+            }
+          }
+        }
+      }
+    )
+    
   })
 
 })
--- src/modules/layered-navigation/util/CreateAggregations.ts
+++ src/modules/layered-navigation/util/CreateAggregations.ts
@@ -1,13 +1,108 @@
-interface Filters {
+interface Aggregations {
   [key: string]: Array<Number> | Array<string>
 }
 
-export default (filters: Filters) => {
+interface Filter {
+  name: string,
+  path: string,
+  aggName: string,
+  value: Array<string> | Array<Number>
+}
 
-  const base = {
-    aggs: {}
+// interface Aggregation {
+//   [key: string]: {
+//     filter: {
+
+//     }
+//   }
+// }
+
+export default class AgregationsCreator {
+
+  private aggregations;
+  private applied;
+
+  constructor(private filters: Array<Filter>) {
+    this.aggregations = {
+      aggs: {}
+    }
+
+    this.applied = filters.reduce((total, { value }) => {
+      if (value && value.length) {
+        total++
+      }
+      return total
+    }, 0)
   }
 
-  return base
+  public appendFilterToAggregation (filterName: string): any {
+
+    const filter = this.filters.find(filter => filter.name === filterName)
+    if (!filter) {
+      throw new Error('Requested filter does not exist')
+    }
+
+    const hasAppliedFilters: Boolean = !!(filter.value && filter.value.length)
+    const fewFacets: Boolean = (this.applied - (hasAppliedFilters ? 1 : 0)) > 1
+
+    const agg: any = {
+      [`${filter.aggName}_wrapper`]: {
+        filter: {
+
+        },
+        aggs: {
+          [filter.aggName]: {
+
+          }
+        }
+      }
+    }
+
+    if (fewFacets) {
+      // Diffrent structure for a few filters
+      agg[`${filter.aggName}_wrapper`].filter = {
+        bool: {
+          must: []
+        }
+      }
+    }
+
+    // Pointer to filters
+    for (let globalFilter of this.filters) {
+
+      if (JSON.stringify(globalFilter) === JSON.stringify(filter)) {
+        continue
+      }
+
+      console.log(globalFilter)
+      
+      if (globalFilter.value && globalFilter.value.length) {
+
+        const condition = {
+          terms: {
+            [globalFilter.path]: globalFilter.value
+          }
+        }
+
+        if(!fewFacets) {
+          agg[`${filter.aggName}_wrapper`].filter = condition
+          break;
+        }
+
+        // Push to array if there are a few
+        agg[`${filter.aggName}_wrapper`].filter.bool.must.push(condition)
+      }
+
+    }
+
+    agg[`${filter.aggName}_wrapper`].aggs[filter.aggName] = {
+      terms: {
+        field: [filter.path]
+      }
+    }
+
+    return agg
+
+  }
 
 }
